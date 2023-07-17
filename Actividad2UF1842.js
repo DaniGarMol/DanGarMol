5dfdcf15555ac1160f75cfb3c1047fe846e7a245
DaniGarMol <correoempleodaniel@gmail.com>
2023-07-17 18:13:30 +0100

    primera subida

---
+++ Actividad2UF1842.js
@@ -0,0 +1,22 @@
+var primerNumero = parseInt ( prompt ("Introduce el primer número: ", "0"));
+console.log ("El primer número es: " + primerNumero);
+var segundoNumero = parseInt (prompt ("Introduce el segundo número: ", "0"));
+console.log ("El segundo número es:" + segundoNumero);
+
+let suma = 0;
+suma = primerNumero + segundoNumero;
+console.log ("El resultado de la suma es: " + suma); 
+// resultado = Number(primerNumero) + Number(segundoNumero);
+
+
+let resta = 0;
+resta = primerNumero - segundoNumero;
+console.log ("El resultado de la resta es " + resta);
+
+let producto = 0;
+producto = primerNumero * segundoNumero;
+console.log ("El resultado del producto es " + producto);
+
+let division = 0;
+division = primerNumero / segundoNumero;
+console.log ("El resultado de la división es " + division);
