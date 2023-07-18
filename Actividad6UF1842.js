7160f9418a08d514065c2f1353a91f125ce6292c
DaniGarMol <correoempleodaniel@gmail.com>
2023-07-18 18:47:01 +0100

    ejercicio6

--- Actividad6UF1842.js
+++ Actividad6UF1842.js
@@ -1,16 +1,16 @@
 var clave = prompt("Introduce una clave de 6 caracteres: ");
 
 if (clave == "123456"){
-    document.write( "Su clave es la correcta. ¡Bienvenido!");
+    alert( "Su clave es la correcta. ¡Bienvenido!");
 }
 
 else {
     for (var n=1; n<=3; n++){
         if( n<3){
-            document.write ("Su clave no es correcta. Intentelo de nuevo." + clave);
+            alert("Su clave no es correcta. Intentelo de nuevo.") + prompt("Introduce una clave de 6 caracteres: ");
         }
         else {
-            document.write("Su acceso está denegado. Póngase en contacto con el administrador.");
+            alert("Su acceso está denegado. Póngase en contacto con el administrador.");
         }
     }
 }
