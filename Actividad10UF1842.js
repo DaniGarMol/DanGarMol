4247a64bcebdd2c8e70db107da298465398ef37e
DaniGarMol <correoempleodaniel@gmail.com>
2023-07-20 15:16:27 +0100

    jueves

--- Actividad10UF1842.js
+++ Actividad10UF1842.js
@@ -1,5 +1,5 @@
 let compra = parseInt(prompt("Introduce el importe de la compra: "));
-alert ("El importe de la compra es: " + compra);
+alert ("El importe de la compra es: " + compra + "€");
 
 let mes = prompt("Introduce el mes: ");
 alert ("La compra se realizó en el mes de " + mes);
@@ -8,36 +8,24 @@ mes = (mes.toLowerCase());
 let descuento;
 
 switch (mes){
-    case "enero":
+    case "enero": case "febrero":
         descuento = compra * 0.20;
-        document.write("Su descuento es del 20%. <br/> El importe de su compra es " + (compra - descuento));
+        document.write("Su descuento es del 20%. <br/> El importe de su compra es " + (compra - descuento) + "€");
         break;
-    case "febrero":
-        descuento = compra * 0.20;
-        document.write("Su descuento es del 20%. <br/> El importe de su compra es " + (compra - descuento));
-        break;
-    case "julio":
+    case "julio": case "agosto":
         descuento = compra * 0.15;
-        document.write("Su descuento es del 15%. <br/> El importe de su compra es " + (compra - descuento));
-        break;
-    case "agosto":
-        descuento = compra * 0.15;
-        document.write("Su descuento es del 15%. <br/> El importe de su compra es " + (compra - descuento));
+        document.write("Su descuento es del 15%. <br/> El importe de su compra es " + (compra - descuento) + "€");
         break;
     case "octubre":
         descuento = compra * 0.15;
-        document.write("Su descuento es del 15%. <br/> El importe de su compra es " + (compra - descuento));
+        document.write("Su descuento es del 15%. <br/> El importe de su compra es " + (compra - descuento) + "€");
         break;
-    case "noviembre":
+    case "noviembre": case "diciembre":
         descuento = compra * 0.25;
-        document.write("Su descuento es del 25%. <br/> El importe de su compra es " + (compra - descuento));
-        break;
-    case "diciembre":
-        descuento = compra * 0.25;
-        document.write("Su descuento es del 25%. <br/> El importe de su compra es " + (compra - descuento));
+        document.write("Su descuento es del 25%. <br/> El importe de su compra es " + (compra - descuento) + "€");
         break;
     default:
-        document.write("El importe de su compra es " + compra);
+        document.write("El importe de su compra es " + compra + "€");
         break;
 
 }
