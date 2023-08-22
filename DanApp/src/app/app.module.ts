07f0704dcaef0badd2dfdb3001bc3327609b509b
iDankest <DankestMirror@hotmail.es>
2023-08-22 18:19:26 +0100

    componentes

--- DanApp/src/app/app.module.ts
+++ DanApp/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoComponent } from './curso/curso.component';
 import { AlumnosComponent } from './alumnos/alumnos.component';
-import { AlumnoComponent } from './alumno/alumno.component';
+import { AlumnoComponent } from './Componentes/alumno/alumno.component';
 import { ProfesoresComponent } from './profesores/profesores.component';
 import { ProfesorComponent } from './profesor/profesor.component';
 import { FormularioAccesoComponent } from './formulario-acceso/formulario-acceso.component';
